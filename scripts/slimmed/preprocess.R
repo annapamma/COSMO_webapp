5d1df19a994331c882749b52ddb573e9cd4adeee
Anna Calinawan <anna.pamela@gmail.com>
2021-01-05 13:05:20 -0500

    add preprocess script

--- scripts/slimmed/preprocess.R
+++ scripts/slimmed/preprocess.R
@@ -1,7 +1,14 @@
-source("../COSMO/bin/tools.R")
+args = commandArgs(trailingOnly=TRUE)
+
+working_dir = args[1]
+pro_file = args[2]
+rna_file = args[3]
+sample_file = args[4]
+source(paste(working_dir, "/scripts", "/COSMO/bin/tools.R", sep = ""))
+
 # pro_file <- "${pro_file}"
 # rna_file <- "${rna_file}"
 # sample_file <- "${sample_file}"
-# out_dir <- "data_use"
-# dir.create(out_dir)
-# format_input_data(pro_file, rna_file, sample_file, out_dir = out_dir)
+out_dir <- "data_use"
+dir.create(out_dir)
+format_input_data(pro_file, rna_file, sample_file, out_dir = out_dir)
